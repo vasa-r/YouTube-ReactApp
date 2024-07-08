6734fc2fac1f2bf260b6097ea353ad1997485805
VasaR <vasanthias52@gmail.com>
2024-07-09 00:37:09 +0530

    autocomplete or search bar is implemented with debouncing. and cache storing has to be done

--- video-tube/src/components/Header/Header.jsx
+++ video-tube/src/components/Header/Header.jsx
@@ -8,13 +8,41 @@ import User from "../../assets/user.svg";
 import styles from "../Header/Header.module.css";
 import { useDispatch } from "react-redux";
 import { menuToggle } from "../../redux/slices/menuSlice.jsx";
+import { AUTOCOMPLETE_API } from "../../utils/constants.jsx";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestion, setShowSuggestion] = useState(false);
+
+  // console.log(searchQuery);
+
   const dispatch = useDispatch();
 
   const handleMenuOpen = () => {
     dispatch(menuToggle());
   };
+
+  useEffect(() => {
+    //debouncing
+    const timer = setTimeout(() => {
+      fetchSuggestion();
+    }, 300);
+
+    //clearing the timeout or sideeffect
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const fetchSuggestion = async () => {
+    const res = await fetch(AUTOCOMPLETE_API + searchQuery);
+    const json = await res.json();
+    // console.log(json[1]);
+    setSuggestions(json[1]);
+  };
+
   return (
     <div className={styles.headerContainer}>
       <div className={styles.menuLogo}>
@@ -29,21 +57,33 @@ const Header = () => {
       <div className={styles.searchMic}>
         <div className="suggestTabInput">
           <div className={styles.search}>
-            <input id={styles.input} type="text" placeholder="Search" />
+            <input
+              id={styles.input}
+              type="text"
+              placeholder="Search"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onFocus={() => {
+                setShowSuggestion(true);
+              }}
+              onBlur={() => setShowSuggestion(false)}
+            />
             <button className={styles.searchBtn}>
               <img src={Search} alt="search-icon" />
             </button>
           </div>
-          <div className={styles.suggestionTab}>
-            <p>
-              <img src={Search} alt="search icon" />
-              vasanth here
-            </p>
-            <p>
-              <img src={Search} alt="search icon" />
-              vasanth here
-            </p>
-          </div>
+          {suggestions.length >= 1 && showSuggestion && (
+            <div className={styles.suggestionTab}>
+              {suggestions.map((suggestion, index) => {
+                return (
+                  <p key={index}>
+                    <img src={Search} alt="search icon" />
+                    {suggestion}
+                  </p>
+                );
+              })}
+            </div>
+          )}
         </div>
 
         <div className={styles.mic}>
--- video-tube/src/utils/constants.jsx
+++ video-tube/src/utils/constants.jsx
@@ -3,3 +3,6 @@ const API_KEY = "";
 export const API_URI =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   API_KEY;
+
+export const AUTOCOMPLETE_API =
+  "https://suggestqueries.google.com/complete/search?client=firefox&q=";
