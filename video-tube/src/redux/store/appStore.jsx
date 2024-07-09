dc828656925f60ee78fc716a4b00dd02bd3e7dff
VasaR <vasanthias52@gmail.com>
2024-07-09 21:29:54 +0530

    chatSlice created for live chat and connected to store

--- video-tube/src/redux/store/appStore.jsx
+++ video-tube/src/redux/store/appStore.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuSlice from "../slices/menuSlice.jsx";
+import chatSlice from "../slices/chatSlice.jsx";
 
 const appStore = configureStore({
   reducer: {
     menu: menuSlice,
+    chat: chatSlice,
   },
 });
 
